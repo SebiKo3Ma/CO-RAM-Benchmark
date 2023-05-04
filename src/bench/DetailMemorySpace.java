e1c7e8dd0aa3d634fce5e2a9885b3636aee4049f
Viv.CD <dumitrescucarla2@gmail.com>
2023-05-04 18:47:25 +0300

    so this code gives the memory detail of the pc. splituit

--- src/bench/DetailMemorySpace.java
+++ src/bench/DetailMemorySpace.java
@@ -1,9 +1,8 @@
 package bench;
+
 import java.lang.management.ManagementFactory;
 import com.sun.management.OperatingSystemMXBean;
-
-public class MemoryBenchmark{
-    public static void main(String[] args) {
+public class DetailMemorySpace {
         OperatingSystemMXBean osBean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
         long totalMemory = osBean.getTotalPhysicalMemorySize();
         long freeMemory = osBean.getFreePhysicalMemorySize();
@@ -11,8 +10,9 @@ public class MemoryBenchmark{
         double usedGB = (double) usedMemory / (1024 * 1024 * 1024);
         double totalGB = (double) totalMemory / (1024 * 1024 * 1024);
         double freeGB = (double) freeMemory / (1024 * 1024 * 1024);
-        System.out.println("Total Memory: " + totalGB + " GB");
-        System.out.println("Used Memory: " + usedGB + " GB");
-        System.out.println("Free Memory: " + freeGB + " GB");
-    }
+        public void printMemoryDetails() {
+                System.out.println("Total Memory: " + totalGB + " GB");
+                System.out.println("Used Memory: " + usedGB + " GB");
+                System.out.println("Free Memory: " + freeGB + " GB");
+        }
 }
