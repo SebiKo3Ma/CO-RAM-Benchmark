7cafd7379b02ed9e452bf110d771a469d6233343
alexa <alexabianca091@gmail.com>
2023-05-25 10:47:38 +0300

    Added limit to the MemoryEaterTest

--- src/bench/MemoryEaterTest.java
+++ src/bench/MemoryEaterTest.java
@@ -1,28 +1,33 @@
 package bench;
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
 import java.util.Vector;
 
 
-
 public class MemoryEaterTest implements IBenchmark{
-
+    OperatingSystemMXBean osBean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
     static int count = 0;
     public void run(){
 
         Vector v = new Vector();
 
         while (true) {
-            try {
-                count++;
-                byte b[] = new byte[4*1048576];
-                v.add(b);
+            long freeMemory = osBean.getFreePhysicalMemorySize();
+            if(freeMemory>=500*1024*1023) {
+                try {
+                    count++;
+                    byte b[] = new byte[4 * 1048576];
+                    v.add(b);
 
-            } catch(OutOfMemoryError e){
-                try{
-                    System.out.println("\nProgram has reached "+ count + " iterations");
-                } catch(OutOfMemoryError f){
-                    ;
-                }
-                break;
+                } catch (OutOfMemoryError e) {
+                    try {
+                        System.out.println("\nProgram has reached " + count + " iterations");
+                    } catch (OutOfMemoryError f) {
+                        ;
+                    }
+                    break;
+                    }
             }
         }
     }
